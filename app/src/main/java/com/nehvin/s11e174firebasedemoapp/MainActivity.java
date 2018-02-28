26fe7f7a93b40ab43b71b44ca91a55922dbc8949
Vineet K. Jain <reachvineetjain@users.noreply.github.com>
2018-02-28 16:02:16 +0530

    completed app with adMob

--- app/src/main/java/com/nehvin/s11e174firebasedemoapp/MainActivity.java
+++ app/src/main/java/com/nehvin/s11e174firebasedemoapp/MainActivity.java
@@ -2,42 +2,46 @@ package com.nehvin.s11e174firebasedemoapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
+
+    AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference();
+        MobileAds.initialize(this, getResources().getString(R.string.banner_ad_unit_id));
 
-        Map<String, String> values = new HashMap<>();
-        values.put("Vineet","Jain");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
-        myRef.push().setValue(values, new DatabaseReference.CompletionListener()
-        {
-            @Override
-            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-                if(databaseError == null){
-                    Log.i("Info", "onComplete: Save Successful");
-                }
-                else
-                {
-                    Log.i("Error", "onComplete: Save unsuccessful");
-                }
-            }
-        });
+//        // Write a message to the database
+//        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference();
+//
+//        Map<String, String> values = new HashMap<>();
+//        values.put("Vineet","Jain");
+//
+//        myRef.push().setValue(values, new DatabaseReference.CompletionListener()
+//        {
+//            @Override
+//            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+//                if(databaseError == null){
+//                    Log.i("Info", "onComplete: Save Successful");
+//                }
+//                else
+//                {
+//                    Log.i("Error", "onComplete: Save unsuccessful");
+//                }
+//            }
+//        });
 //        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
 
     }
