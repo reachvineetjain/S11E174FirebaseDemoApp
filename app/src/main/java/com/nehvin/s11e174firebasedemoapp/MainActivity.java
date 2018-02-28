4116d757abcc0b237d5fa92fd999b482e1679f99
Vineet K. Jain <reachvineetjain@users.noreply.github.com>
2018-02-28 16:17:06 +0530

    completed app with adMob

--- app/src/main/java/com/nehvin/s11e174firebasedemoapp/MainActivity.java
+++ app/src/main/java/com/nehvin/s11e174firebasedemoapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.nehvin.s11e174firebasedemoapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
@@ -21,6 +22,35 @@ public class MainActivity extends AppCompatActivity {
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
 
 //        // Write a message to the database
 //        final FirebaseDatabase database = FirebaseDatabase.getInstance();
